8960b815fef1e63bf60ab06565fc6e3012f40850
jwchoi <iamjwchoi@gmail.com>
2020-01-16 20:01:42 +0900

    Add a functionality for processing sh:node

--- src/main/java/shaper/mapping/model/shacl/PropertyShape.java
+++ src/main/java/shaper/mapping/model/shacl/PropertyShape.java
@@ -6,28 +6,45 @@ import shaper.mapping.model.r2rml.PredicateMap;
 import shaper.mapping.model.r2rml.RefObjectMap;
 
 public class PropertyShape extends Shape {
+    private enum MappingTypes { OBJECT_MAP, REF_OBJECT_MAP }
+
     private PredicateMap mappedPredicateMap;
     private ObjectMap mappedObjectMap;
     private RefObjectMap mappedRefObjectMap;
+
+    private MappingTypes mappingType;
 
     PropertyShape(IRI id, PredicateMap mappedPredicateMap, ObjectMap mappedObjectMap, ShaclDocModel shaclDocModel) {
         super(id, shaclDocModel);
         this.mappedPredicateMap = mappedPredicateMap;
         this.mappedObjectMap = mappedObjectMap;
+
+        mappingType = MappingTypes.OBJECT_MAP;
     }
 
     PropertyShape(IRI id, PredicateMap mappedPredicateMap, RefObjectMap mappedRefObjectMap, ShaclDocModel shaclDocModel) {
         super(id, shaclDocModel);
         this.mappedPredicateMap = mappedPredicateMap;
         this.mappedRefObjectMap = mappedRefObjectMap;
+
+        mappingType = MappingTypes.REF_OBJECT_MAP;
     }
 
     @Override
     public String toString() {
         String serializedPropertyShape = getSerializedShape();
-        if (serializedPropertyShape != null) return serializedPropertyShape;
+        if (serializedPropertyShape == null) {
+            serializedPropertyShape = buildSerializedPropertyShape();
+            setSerializedShape(serializedPropertyShape);
+        }
 
+        return serializedPropertyShape;
+    }
+
+    private String buildSerializedPropertyShape() {
         StringBuffer buffer = new StringBuffer();
+
+        String o; // to be used as objects of different RDF triples
 
         String id = getShaclDocModel().getRelativeIRI(getID().toString());
         buffer.append(id);
@@ -36,15 +53,19 @@ public class PropertyShape extends Shape {
         buffer.append(getPO(Symbols.A, "sh:PropertyShape"));
         buffer.append(getSNT());
 
-        String p = getShaclDocModel().getRelativeIRI(mappedPredicateMap.getConstant().get());
-        buffer.append(getPO("sh:path", p));
+        o = getShaclDocModel().getRelativeIRI(mappedPredicateMap.getConstant().get());
+        buffer.append(getPO("sh:path", o));
         buffer.append(getSNT());
+
+        if (mappingType.equals(MappingTypes.REF_OBJECT_MAP)) {
+            o = getShaclDocModel().getRelativeIRI(mappedRefObjectMap.getParentTriplesMap().toString());
+            buffer.append(getPO("sh:node", o));
+            buffer.append(getSNT());
+        }
 
         buffer.setLength(buffer.lastIndexOf(Symbols.SEMICOLON));
         buffer.append(getDNT());
 
-        serializedPropertyShape = buffer.toString();
-        setSerializedShape(serializedPropertyShape);
-        return serializedPropertyShape;
+        return buffer.toString();
     }
 }
