fe0907b1b806175b6ec2be27b068cc88172db86a
jwchoi <iamjwchoi@gmail.com>
2020-01-17 16:17:23 +0900

    Add a functionality for processing cardinality constraint

--- src/main/java/shaper/mapping/model/shacl/PropertyShape.java
+++ src/main/java/shaper/mapping/model/shacl/PropertyShape.java
@@ -1,9 +1,15 @@
 package shaper.mapping.model.shacl;
 
+import janus.database.SQLSelectField;
 import shaper.mapping.Symbols;
 import shaper.mapping.model.r2rml.ObjectMap;
 import shaper.mapping.model.r2rml.PredicateMap;
 import shaper.mapping.model.r2rml.RefObjectMap;
+import shaper.mapping.model.r2rml.Template;
+
+import java.sql.ResultSetMetaData;
+import java.util.List;
+import java.util.Optional;
 
 public class PropertyShape extends Shape {
     private enum MappingTypes { OBJECT_MAP, REF_OBJECT_MAP }
@@ -41,11 +47,72 @@ public class PropertyShape extends Shape {
         return serializedPropertyShape;
     }
 
+    private String buildSerializedPropertyShape(RefObjectMap mappedRefObjectMap) {
+        StringBuffer buffer = new StringBuffer();
+
+        String o; // to be used as objects of different RDF triples
+
+        // sh:node
+        o = getShaclDocModel().getRelativeIRI(mappedRefObjectMap.getParentTriplesMap().toString());
+        buffer.append(getPO("sh:node", o));
+        buffer.append(getSNT());
+
+        return buffer.toString();
+    }
+
+    private String buildSerializedPropertyShape(ObjectMap mappedObjectMap) {
+        StringBuffer buffer = new StringBuffer();
+
+        String o; // to be used as objects of different RDF triples
+
+        // cardinality
+        o = "1";
+        Optional<SQLSelectField> sqlSelectField = mappedObjectMap.getColumn();
+        if (sqlSelectField.isPresent()) {
+            switch (sqlSelectField.get().getNullable()) {
+                case ResultSetMetaData.columnNoNulls:
+                    // "exactly one"
+                    buffer.append(getPO("sh:minCount", o));
+                    buffer.append(getSNT());
+                case ResultSetMetaData.columnNullable:
+                case ResultSetMetaData.columnNullableUnknown:
+                    // "zero or one"
+                    buffer.append(getPO("sh:maxCount", o));
+                    buffer.append(getSNT());
+            }
+        } else {
+            Optional<Template> template = mappedObjectMap.getTemplate();
+            if (template.isPresent()) {
+                List<SQLSelectField> columnNames = template.get().getColumnNames();
+                boolean isEveryColumnNoNulls = true;
+                for (SQLSelectField columnName: columnNames) {
+                    // "?" - zero or one
+                    if (columnName.getNullable() != ResultSetMetaData.columnNoNulls) {
+                        buffer.append(getPO("sh:maxCount", o));
+                        buffer.append(getSNT());
+                        isEveryColumnNoNulls = false;
+                        break;
+                    }
+                }
+                // "exactly one"
+                if (isEveryColumnNoNulls) {
+                    buffer.append(getPO("sh:minCount", o));
+                    buffer.append(getSNT());
+                    buffer.append(getPO("sh:maxCount", o));
+                    buffer.append(getSNT());
+                }
+            }
+        }
+
+        return buffer.toString();
+    }
+
     private String buildSerializedPropertyShape() {
         StringBuffer buffer = new StringBuffer();
 
         String o; // to be used as objects of different RDF triples
 
+        // rdf:type sh:PropertyShape
         String id = getShaclDocModel().getRelativeIRI(getID().toString());
         buffer.append(id);
         buffer.append(getNT());
@@ -53,15 +120,18 @@ public class PropertyShape extends Shape {
         buffer.append(getPO(Symbols.A, "sh:PropertyShape"));
         buffer.append(getSNT());
 
+        // sh:path
         o = getShaclDocModel().getRelativeIRI(mappedPredicateMap.getConstant().get());
         buffer.append(getPO("sh:path", o));
         buffer.append(getSNT());
 
-        if (mappingType.equals(MappingTypes.REF_OBJECT_MAP)) {
-            o = getShaclDocModel().getRelativeIRI(mappedRefObjectMap.getParentTriplesMap().toString());
-            buffer.append(getPO("sh:node", o));
-            buffer.append(getSNT());
-        }
+        // if RefObjectMap
+        if (mappingType.equals(MappingTypes.REF_OBJECT_MAP))
+            buffer.append(buildSerializedPropertyShape(mappedRefObjectMap));
+
+        // if ObjectMap
+        if (mappingType.equals(MappingTypes.OBJECT_MAP))
+            buffer.append(buildSerializedPropertyShape(mappedObjectMap));
 
         buffer.setLength(buffer.lastIndexOf(Symbols.SEMICOLON));
         buffer.append(getDNT());
